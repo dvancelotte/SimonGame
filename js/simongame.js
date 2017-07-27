a8c6888258bd5a47cd494f95656dc1330391d6fb
Deborah Vancelotte <vancelotted@gmail.com>
2017-07-26 22:03:56 -0300

    continue

--- js/simongame.js
+++ js/simongame.js
@@ -1,4 +1,7 @@
 memoryCount = 0;
+captionGameCount = 0;
+memoryArray = [];
+
 
 foxs = [
         {id:"fox1", sound:"https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"},
@@ -15,36 +18,78 @@ function PlayMemory(){
     memoryCount ++;
     
     memoryArray = creatMemory(memoryCount);
+    fox = 0;
     
-
-    for (fox in memoryArray){
-        
-        console.log(fox);
-        
-        $("#"+foxs[fox].id).animate({
-            width: '25%'
-          },'fast', function() {
-            var audio = new Audio(foxs[fox].sound);
-            audio.play();
-            $("#"+foxs[fox].id).animate({width: '20%'});
-        });       
+    var loopfox = function(memoryArray){
+       
+        animateFox(memoryArray[fox],function(){
+            fox++;
             
+            if(fox<memoryArray.length){
+                loopfox(memoryArray);
+            }else{
+                refreshInput(false);
+            }
+        });
+    };
+    
+    function animateFox(memoryArray,callback){      
+        
+        $("#"+foxs[memoryArray].id).animate({opacity: '1.0'});
+        var audio = new Audio(foxs[memoryArray].sound);
+        $("#"+foxs[memoryArray].id).animate({opacity: '0.4'});            
+        audio.play();
+        audio.onended = function() {
+            callback();
+        };
+        
     }
+    
+    loopfox(memoryArray);
+   
+
     
 }
 
-function creatMemory(memoryCount){
-    
-    array =[];
-    
+function creatMemory(memoryCount){    
+    array =[];    
     for (i=0; i<memoryCount; i++){
         array.push(Math.floor(Math.random() * 4));        
-    }
-    
-    return array;
-    
+    }    
+    return array;    
 }
 
+function refreshInput(flag){
+    $("#fox1").prop('disabled', flag);
+    $("#fox2").prop('disabled', flag);
+    $("#fox3").prop('disabled', flag);
+    $("#fox4").prop('disabled', flag);
+    
+    if(flag){
+            memoryCount = 0;
+            captionGameCount = 0;
+            memoryArray = [];    
+    }
+}
+
+function captionGame(numberFox){
+    
+    if (captionGameCount<memoryCount){
+        if(numberFox==memoryArray[captionGameCount]){
+            captionGameCount ++;
+            document.getElementById("Message").innerHTML = "Great";
+            if(captionGameCount==memoryCount){
+                refreshInput(true);
+                document.getElementById("Message").innerHTML = "good";
+            }
+        }
+        else{
+            refreshInput(true);
+            document.getElementById("Message").innerHTML = "You lose :(";
+        }
+        
+    }
+}
 
 $(document).ready(function() {
      
